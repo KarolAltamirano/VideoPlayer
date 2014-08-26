54fdea8415a34f7685e12c7c3c8bb058b4bdc924
Karol Altamirano <karlos.altamirano@gmail.com>
2014-08-26 20:10:20 +0200

    Added progress bar, created player template

--- script/videoPlayer.js
+++ script/videoPlayer.js
@@ -1,20 +1,68 @@
+var Player = function (input) {
+    var playerTemplate;
+    /*
+    input = 
+        {
+            element: string, 
+            width: number, 
+            src: { mp4: string, webm: string, ogv: string},
+            annotations: { annotation: { time: string, description: string } }
+        }
+    */
 
-var Player = function (id, width) {
-    this.playerBox = document.getElementById(id);
+    playerTemplate = this.template(input.src.mp4, input.src.webm, input.src.ogv)
+
+    this.playerBox = document.getElementById(input.element);
+
+    this.playerBox.innerHTML = playerTemplate;
+
+
+
     this.video = this.playerBox.getElementsByTagName('video')[0];
     this.videoPlay = this.playerBox.getElementsByClassName('playButton')[0];
     this.videoBg = this.playerBox.getElementsByClassName('playBg')[0];
     
     this.addEvents();
+
+    this.playerBox.style.width = input.width + 'px';
+    this.video.style.width = input.width + 'px';
+};
+
+Player.prototype.template = function (mp4, webm, ogv) {
+    var playerTemplate;
+    playerTemplate =
+        '<div class="videoBox">' +
+            '<video>' +
+                '<source src="' + mp4 + '" type="video/mp4">' +
+                '<source src="' + webm + '" type="video/webm">' +
+                '<source src="' + ogv + '" type="video/ogg">' +
+                'Your browser does not support the video player.' +
+            '</video>' +
+            '<div class="playButton"></div>' +
+            '<div class="playBg"></div>' +
+        '</div>' +
+        '<div class="control">' +
+            '<div class="progressBar">' +
+                '<div class="progressBarIn"></div>' +
+                '<div class="ProgressBarLoad"></div>' +
+                '<div class="progressBarBase"></div>' +
+                '<div class="dot"></div>' +
+                '<div class="anot"></div>' +
+            '</div>' +
+        '</div>';
+    return playerTemplate;
+};
+
+Player.prototype.annotations = function (annot) {
     
-    this.playerBox.style.width = width + 'px';
-    this.video.style.width = width + 'px';
 };
 
 Player.prototype.addEvents = function () {
     this.video.onclick = (function (_this) { return function () { _this.playPause(); }; })(this);
     this.videoPlay.onclick = (function (_this) { return function () { _this.playPause(); }; })(this);
     this.videoBg.onclick = (function (_this) { return function () { _this.playPause(); }; })(this);
+    this.video.addEventListener('timeupdate', (function (_this) { return function () { _this.progressBar(); }; })(this));
+    this.video.addEventListener('progress', (function (_this) { return function () { _this.loadProgress(); }; })(this));
 };
 
 Player.prototype.playPause = function () {
@@ -30,6 +78,22 @@ Player.prototype.playPause = function () {
     }
 };
 
-document.addEventListener('DOMContentLoaded', function () {
-    player = new Player('video_1', 500);
-});
+Player.prototype.loadProgress = function () {
+    var bar, actualPos;
+
+    bar = this.playerBox.getElementsByClassName('ProgressBarLoad')[0];
+    actualPos = (this.video.buffered.end(0) / this.video.duration) * 100;
+    bar.style.width = actualPos + '%';
+};
+
+Player.prototype.progressBar = function () {
+    var bar, actualPos, dot;
+    dot = this.playerBox.getElementsByClassName('dot')[0];
+    bar = this.playerBox.getElementsByClassName('progressBarIn')[0];
+    actualPos = (this.video.currentTime / this.video.duration) * 100;
+    bar.style.width = actualPos + '%';
+    dot.style.left = actualPos + '%';
+
+};
+
+
