8c4c33438cd08cf4bccb6b051dffae7cccfd3e0d
Karol Altamirano <karlos.altamirano@gmail.com>
2014-08-27 12:05:48 +0200

    Defined css global prefix, added jquery library, optimalized url change on hotspots, code improvements

--- script/videoPlayer.js
+++ script/videoPlayer.js
@@ -1,31 +1,64 @@
+/*******************************************************************************
+ *                                                                             *
+ *  Title:     Video Player                                                    *
+ *  Author:    Karol Altamirano                                                *
+ *  Version:   0.1                                                             *
+ *                                                                             *
+ *******************************************************************************/
+
+/***************************************************************************************    
+ *                                                                                     *
+ * Player - constructor of the player object                                           *
+ * Player is global variable to create and manage the player                           *
+ *  input =                                                                            *
+ *      {                                                                              *
+ *          element:     string,                                                       *
+ *          width:       number,                                                       *
+ *          src:         { mp4: string, webm: string, ogv: string},                    *
+ *          annotations: { annotation: { time: string, description: string } },        *
+ *          useHash:     boolean                                                       *
+ *      }                                                                              *
+ *                                                                                     *
+ ***************************************************************************************/
+
 var Player = function (input) {
     var playerTemplate;
-    /*
-    input = 
-        {
-            element: string, 
-            width: number, 
-            src: { mp4: string, webm: string, ogv: string},
-            annotations: { annotation: { time: string, description: string } }
-        }
-    */
+    // test input variables
+    if (input.element === undefined) {
+        console.log('Error: set element variable');
+        return false;
+    }
+    if (input.width === undefined) {
+        console.log('Error: set width variable');
+        return false;
+    }
+    if (input.src.mp4 === undefined && input.src.webm === undefined && input.src.ogv === undefined) {
+        console.log('Error: set video src variable');
+        return false;
+    }
 
-    // process input data
+    // set variables after the test
+    if (input.useHash === true) {
+        this.useHash = true;
+    } else {
+        this.useHash = false;
+    }
+
     this.element = input.element;
     this.annots = input.annotations;
     this.width = input.width;
 
     // create player template
-    playerTemplate = this.template(input.src.mp4, input.src.webm, input.src.ogv)
+    playerTemplate = this.template(input.src.mp4, input.src.webm, input.src.ogv);
 
-    // write template to the doc
+    // write template to the page
     this.playerBox = document.getElementById(input.element);
     this.playerBox.innerHTML = playerTemplate;
 
     // select player elements
     this.video = this.playerBox.getElementsByTagName('video')[0];
-    this.videoPlay = this.playerBox.getElementsByClassName('playButton')[0];
-    this.videoBg = this.playerBox.getElementsByClassName('playBg')[0];
+    this.videoPlay = this.playerBox.getElementsByClassName('VP_playButton')[0];
+    this.videoBg = this.playerBox.getElementsByClassName('VP_playBg')[0];
     
     // setting player
     this.addEvents();
@@ -41,34 +74,39 @@ Player.prototype.cssStyle = function () {
 Player.prototype.template = function (mp4, webm, ogv) {
     var playerTemplate;
     playerTemplate =
-        '<div class="videoBox">' +
+        '<div class="VP_videoBox">' +
             '<video>' +
-                '<source src="' + mp4 + '" type="video/mp4">' +
-                '<source src="' + webm + '" type="video/webm">' +
-                '<source src="' + ogv + '" type="video/ogg">' +
+                (mp4  !== undefined ? '<source src="' + mp4  + '" type="video/mp4">'  : '') +
+                (webm !== undefined ? '<source src="' + webm + '" type="video/webm">' : '') +
+                (ogv  !== undefined ? '<source src="' + ogv  + '" type="video/ogg">'  : '') +
                 'Your browser does not support the video player.' +
             '</video>' +
-            '<div class="playButton"></div>' +
-            '<div class="playBg"></div>' +
+            '<div class="VP_playButton"></div>' +
+            '<div class="VP_playBg"></div>' +
         '</div>' +
-        '<div class="control">' +
-            '<div class="progressBar">' +
-                '<div class="progressBarIn"></div>' +
-                '<div class="ProgressBarLoad"></div>' +
-                '<div class="progressBarBase"></div>' +
-                '<div class="dot"></div>' +
-                '<div class="annots"></div>' +
+        '<div class="VP_control">' +
+            '<div class="VP_progressBar">' +
+                '<div class="VP_progressBarIn"></div>' +
+                '<div class="VP_progressBarLoad"></div>' +
+                '<div class="VP_progressBarBase"></div>' +
+                '<div class="VP_dot"></div>' +
+                '<div class="VP_annots"></div>' +
             '</div>' +
-            '<div class="annoutsBoxes"></div>' +
-        '</div>' +
-        '<canvas id="canvasid" width="100" height="100"></canvas>';
+            '<div class="VP_annoutsBoxes"></div>' +
+        '</div>';
     return playerTemplate;
 };
 
 Player.prototype.annotations = function () {
-    var a, seconds, totalTime, position, annotElement, annotsDots, annotBoxes, oneAnnotDes;
+    var a,              // splited time in format hh:mm:ss to the array
+        seconds,        // position of the hotspon in seconds
+        totalTime,      // total duration of the video
+        position,       // position of the hotspot in % on timeline
+        annotElement,   // parent element of all hotspots on timeline
+        annotBoxes;     // parent element of all annotations
     
-    annotBoxes = this.playerBox.getElementsByClassName('annoutsBoxes')[0];
+    annotElement = this.playerBox.getElementsByClassName('VP_annots')[0];
+    annotBoxes = this.playerBox.getElementsByClassName('VP_annoutsBoxes')[0];
 
     this.annots.forEach( function (e) {
         // calculate time to seconds        
@@ -78,25 +116,74 @@ Player.prototype.annotations = function () {
 
         position = (seconds / totalTime) * 100;
 
-        annotElement = this.playerBox.getElementsByClassName('annots')[0];
-        annotElement.innerHTML = annotElement.innerHTML + '<div class="annotsDots" time="' + seconds + '" style="left: ' + position + '%;"></div>';
-
-        annotBoxes.innerHTML += '<div id="' + this.element + '-' + seconds + '" style="left: ' + position + '%">' + e.description + '</div>';
+        annotElement.innerHTML += 
+            '<div class="VP_annotsDots" time="' + seconds + '" style="left: ' + position + '%;"></div>';
+    
+        annotBoxes.innerHTML += 
+            '<div id="VP_' + this.element + '-' + seconds + '" style="left: ' + (position - 1.5) + '%">' + 
+                '<span id="VP_frame' + this.element + '-' + seconds + '"></span>' +
+                '<hr>' +
+                e.description + 
+            '</div>';
     }, this);
     
-    annotsDots = this.playerBox.getElementsByClassName('annots')[0];
-    annotsDots.addEventListener('click', (function (_this) { return function (e) { _this.playBackPosition(e); }; })(this));
-    annotsDots.addEventListener('mouseover', (function (_this) { return function (e) { _this.annotOver(e); }; })(this));
-    annotsDots.addEventListener('mouseout', (function (_this) { return function (e) { _this.annotOut(e); }; })(this));    
+    
+    annotElement.addEventListener('click', (function (_this) { return function (e) { _this.playBackPosition(e); }; })(this));
+    annotElement.addEventListener('mouseover', (function (_this) { return function (e) { _this.annotOver(e); }; })(this));
+    annotElement.addEventListener('mouseout', (function (_this) { return function (e) { _this.annotOut(e); }; })(this));    
 };
 
 Player.prototype.playBackPosition = function (e) {
-    var timePlayBack, url;
+    var timePlayBack, url, oldUrl, parsed, testLast;
     if (e.target !== e.currentTarget) {  // clicked on the annots dot
         timePlayBack = e.target.getAttribute('time');
         this.video.currentTime = timePlayBack;
-        url = 'video-' + this.element + '-' + timePlayBack;
+
+        // check and change url
+        if (this.useHash) { // used hash in url
+            url = window.location.pathname + '#vpvideo-' + this.element + '-' + timePlayBack;
+        } else { // hash not used in url
+            testLast = window.location.pathname.slice(-1);
+            if (testLast !== '/') { // last character in url is not slash
+                oldUrl = window.location.pathname;
+                parsed = oldUrl.split("/");
+                parsed = parsed.pop().split("-");
+                if (parsed[0] == 'vpvideo') { // there is a video tag in url
+                    url = 'vpvideo-' + this.element + '-' + timePlayBack;
+                } else {
+                    url = oldUrl + '/vpvideo-' + this.element + '-' + timePlayBack;
+                }
+            } else {
+                url = window.location.pathname + 'vpvideo-' + this.element + '-' + timePlayBack;
+            }
+        }
+
         window.history.replaceState({p: url}, '', url);
+    }
+};
+
+Player.prototype.checkUrl = function () {
+    var url, parsed, time;
+
+     if (this.useHash) { // used hash in url
+        url = window.location.hash;
+        parsed = url.split('-');
+        if (parsed[0] == '#vpvideo') {
+            if (parsed[1] == this.element) {
+                time = parseInt(parsed[2]);
+                this.video.currentTime = time;
+            }
+        }
+    } else { // hash not used in url
+        url = window.location.pathname;
+        parsed = url.split("/");
+        parsed = parsed.pop().split("-");
+        if (parsed[0] == 'vpvideo') { // there is a video tag in url
+            if (parsed[1] == this.element) {
+                time = parseInt(parsed[2]);
+                this.video.currentTime = time;
+            }
+        }
     }
 };
 
@@ -105,10 +192,10 @@ Player.prototype.annotOver = function (e) {
     if (e.target !== e.currentTarget) {
         timePlayBack = e.target.getAttribute('time');
         
-        boxId = this.element + '-' + timePlayBack;
+        boxId = 'VP_' + this.element + '-' + timePlayBack;
         
         annotsBox = document.getElementById(boxId);
-        annotsBox.style.display = 'block';
+        $(annotsBox).fadeIn();
     }
 };
 
@@ -117,10 +204,10 @@ Player.prototype.annotOut = function (e) {
     if (e.target !== e.currentTarget) {
         timePlayBack = e.target.getAttribute('time');
         
-        boxId = this.element + '-' + timePlayBack;
+        boxId = 'VP_' + this.element + '-' + timePlayBack;
         
         annotsBox = document.getElementById(boxId);
-        annotsBox.style.display = 'none';
+        $(annotsBox).fadeOut();
     }
 };
 
@@ -141,17 +228,6 @@ Player.prototype.addEvents = function () {
     );
 };
 
-Player.prototype.checkUrl = function () {
-    var url, parsed, time;
-    url = window.location.pathname;
-    parsed = url.split("/");
-    parsed = parsed.pop().split("-");
-    if (parsed[1] == this.element) {
-        time = parseInt(parsed[2]);
-        this.video.currentTime = time;
-    }
-};
-
 Player.prototype.playPause = function () {
     if(this.video.paused) {
         this.videoPlay.style.display = 'none';
@@ -168,15 +244,15 @@ Player.prototype.playPause = function () {
 Player.prototype.loadProgress = function () {
     var bar, actualPos;
 
-    bar = this.playerBox.getElementsByClassName('ProgressBarLoad')[0];
+    bar = this.playerBox.getElementsByClassName('VP_progressBarLoad')[0];
     actualPos = (this.video.buffered.end(0) / this.video.duration) * 100;
     bar.style.width = actualPos + '%';
 };
 
 Player.prototype.progressBar = function () {
     var bar, actualPos, dot;
-    dot = this.playerBox.getElementsByClassName('dot')[0];
-    bar = this.playerBox.getElementsByClassName('progressBarIn')[0];
+    dot = this.playerBox.getElementsByClassName('VP_dot')[0];
+    bar = this.playerBox.getElementsByClassName('VP_progressBarIn')[0];
     actualPos = (this.video.currentTime / this.video.duration) * 100;
     bar.style.width = actualPos + '%';
     dot.style.left = actualPos + '%';
